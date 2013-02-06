89641d4388495d1bf031c25e4fdcc89980d8b766
simson <simson@thesimson.net>
2013-02-07 00:37:51 +0100

    Updated the ratrun members Updated main script to do more voodo

--- ghostlands.js
+++ ghostlands.js
@@ -7,13 +7,22 @@
 
 var goall=function(rosterdata) {
     for (i = 0;i < rosterdata.roster.length;i++) {        
-        go(rosterdata.roster[i].member);
+        go(rosterdata.roster[i]);
     }
 }
 
 
-var go=function(toon) {
-    document.write('<div id='+toon+' class="post"><h2>'+toon+'</h2>Loading</div>');
+var go=function(obj) {
+    var toon=obj.member;
+    var s="dps";
+
+    if(obj.spec){
+      s=obj.spec;
+    };
+    if(obj.os){
+      s=s+'/'+obj.os;
+    };
+    document.write('<div id='+toon+' class="post"><h2>'+toon+'</h2>Loading<form id=spec'+toon+'><input type=hidden name=spec value='+s+'></form></div>');
     var s = document.createElement('script');
     s.src = "http://eu.battle.net/api/wow/character/ghostlands/"+toon+"?fields=titles&jsonp=bnetreturn";
     s.type = "text/javascript";
@@ -28,7 +37,7 @@ var bnetreturn=function(o) {
             s=sprintf( o.titles[i].name,toon);
         }      
     }
-    var specs; 
+    var specs=document.forms["spec"+toon]["spec"].value;
     document.getElementById(toon).innerHTML = "<h2>"+s+"</h2>"
     document.getElementById(toon).innerHTML += '<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/><img src=http://eu.battle.net/static-render/eu/'+o.thumbnail+' alt=img></a>';
     document.getElementById(toon).innerHTML += ' <br/>'+ specs +'<br/>';
