7486d5b558f4c954cdc382f7d7e0f72219831021
apokryfos <apokryfos@outlook.com>
2013-02-07 15:01:21 +0000

    Added icons and some bug fixes
    Fixed: API calls not returning
    Removed: Script inclusion
    Changed: Links now have images rather than text
    Not fixed: Footer (resized a bit but that in itself does not fix any problems)

--- ghostlands.js
+++ ghostlands.js
@@ -1,22 +1,13 @@
-{
-    var ssp = document.createElement('script');
-    ssp.src = "sprintf.js";
-    ssp.type = "text/javascript";
-    document.getElementsByTagName("head")[0].appendChild( ssp);
-};
-
-
 var goall=function(rosterdata) {
     for (i = 0;i < rosterdata.roster.length;i++) {        
         go(rosterdata.roster[i].member);        
     }
 }
 
-
 var go=function(toon) {
     document.write('<div id='+toon+' class="post"><h2>'+toon+'</h2>Loading</div>');
     var s = document.createElement('script');
-    s.src = "http://eu.battle.net/api/wow/character/ghostlands/"+toon+"?fields=titles,talents&jsonp=bnetreturn";
+    s.src = "http://eu.battle.net/api/wow/character/ghostlands/"+toon+"?fields=titles,talents&jsonp=bnetreturn";    
     s.type = "text/javascript";
     document.getElementsByTagName("head")[0].appendChild(s);
 };
@@ -30,7 +21,7 @@ var bnetreturn=function(o) {
         }      
     }
     var specs="";
-    if (o.talents.length == 2 && o.talents[0].spec.role != o.talents[1].spec.role)      {
+    if (typeof o.talents[1].spec != 'undefined' && o.talents[0].spec.role != o.talents[1].spec.role)      {
         specs = o.talents[0].spec.role +  "/" + o.talents[1].spec.role;
     }
     else  {
@@ -42,8 +33,8 @@ var bnetreturn=function(o) {
     document.getElementById(toon).innerHTML = "<h2>"+s+"</h2>"
     document.getElementById(toon).innerHTML += '<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/><img src=http://eu.battle.net/static-render/eu/'+o.thumbnail+' alt=img></a>';
     document.getElementById(toon).innerHTML += ' <br/>'+ specs +'<br/>';
-    document.getElementById(toon).innerHTML += '<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/>WoW Heroes</a><br/>';
-    document.getElementById(toon).innerHTML += ' <a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'>AskMr. Robot</a>';
+    document.getElementById(toon).innerHTML += '<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/><img src="wowheroes.png" alt="WoW-Heroes" title="WoW-Heroes" width="32px" height="32px"/></a>&nbsp;';
+    document.getElementById(toon).innerHTML += ' <a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'><img src="teamrobot.png" alt="Ask Mr. Robot" title="Ask Mr. Robot" width="32px" height="32px"/></a>';
 };
 
 
