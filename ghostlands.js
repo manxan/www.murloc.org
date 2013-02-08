1fde5e2d688a10136985f0b7f791bacc4f49da95
apokryfos <apokryfos@outlook.com>
2013-02-08 13:49:51 +0000

    Javascript and roster updates
    JS: Removed the "hack" with a (in my opinion) better way to do things.
    Changed spec and offspec to numberics: 0 is MDPS, 1 is RDPS, 2 is Healer, 3 is Tank
    Default is 1 if spec isn't there.
    
    Added support for notes

--- ghostlands.js
+++ ghostlands.js
@@ -6,15 +6,34 @@ var goall=function(rosterdata) {
 
 var go=function(obj) {
     var toon=obj.member;
-    var s="dps";
+    
+    var specarr =  [ "MDPS", "RDPS", "Healer", "Tank" ];
+    
+    var note = '';
+    if (typeof obj.note != 'undefined')
+        note = obj.note;
+    
+    var s=specarr[1];
 
     if(obj.spec){
-      s=obj.spec;
+      s=specarr[obj.spec];
     };
     if(obj.os){
-      s=s+'/'+obj.os;
+      s=s+'/'+specarr[obj.os];
     };
-    document.write('<div id='+toon+' class="post"><h2>'+toon+'</h2>Loading<form id=spec'+toon+'><input type=hidden name=spec value='+s+'></form></div>');
+    document.write(
+          '<div id='+toon+' class="post">'
+        + '<div id="Loading'+toon+'">'
+        +'<h2>'+toon+'<h2>'
+        +'<img src="placeholder.png" alt=img>'
+        +'</div>'
+        +s    
+        +'<br/><a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/>'
+        +'<img src="wowheroes.png" alt="WoW-Heroes" title="WoW-Heroes" width="32px" height="32px"/></a>&nbsp;'
+        +'<a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'><img src="teamrobot.png" alt="Ask Mr. Robot" title="Ask Mr. Robot" width="32px" height="32px"/></a>' 
+        +(note!=''?'<br/>'+note:'')
+        +'</div>'
+    );
     var s = document.createElement('script');
     s.src = "http://eu.battle.net/api/wow/character/ghostlands/"+toon+"?fields=titles&jsonp=bnetreturn";
     s.type = "text/javascript";
@@ -29,13 +48,10 @@ var bnetreturn=function(o) {
         if(o.titles[i].selected){
             s=sprintf( o.titles[i].name,toon);
         }      
-    }
-    var specs=document.forms["spec"+toon]["spec"].value;
-    document.getElementById(toon).innerHTML = "<h2>"+s+"</h2>"
-    document.getElementById(toon).innerHTML += '<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/><img src=http://eu.battle.net/static-render/eu/'+o.thumbnail+' alt=img></a>';
-    document.getElementById(toon).innerHTML += ' <br/>'+ specs +'<br/>';
-    document.getElementById(toon).innerHTML += '<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/><img src="wowheroes.png" alt="WoW-Heroes" title="WoW-Heroes" width="32px" height="32px"/></a>&nbsp;';
-    document.getElementById(toon).innerHTML += ' <a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'><img src="teamrobot.png" alt="Ask Mr. Robot" title="Ask Mr. Robot" width="32px" height="32px"/></a>';
+    }    
+    document.getElementById('Loading'+toon).innerHTML = '<h2>'+s+'</h2><a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/><img src=http://eu.battle.net/static-render/eu/'+o.thumbnail+' alt=img></a>';
+        
+    
 };
 
 
