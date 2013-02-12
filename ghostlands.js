be69c0d2e890d7f5b7cb4d56053f45b10c9a7ce8
simson <simson@thesimson.net>
2013-02-12 10:56:06 +0100

    Changed size of icons and the mouse over message closes #7

--- ghostlands.js
+++ ghostlands.js
@@ -25,10 +25,10 @@ var go=function(obj) {
         +'</div>'
         +s    
         +'<br/><a href="http://eu.battle.net/wow/en/character/Ghostlands/'+toon+'/advanced">'
-        +'<img src="blizz.png" alt="battle.net" title="Battle.net" width="57px" height="26px"/></a>&nbsp;'
+        +'<img src="blizz.png" alt="battle.net" title="Official profile of '+toon+' in the wow armoray at Battle.net" width="28px" height="13px"/></a>&nbsp;'
         +'<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/>'
-        +'<img src="wowheroes.png" alt="WoW-Heroes" title="WoW-Heroes" width="48px" height="26px"/></a>&nbsp;'
-        +'<a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'><img src="teamrobot.png" alt="Ask Mr. Robot" title="Ask Mr. Robot" width="32px" height="26px"/></a>' 
+        +'<img src="wowheroes.png" alt="WoW-Heroes" title="Profile of '+toon+' at WoW-Heroes" width="24px" height="13px"/></a>&nbsp;'
+        +'<a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'><img src="teamrobot.png" alt="Ask Mr. Robot" title="Ask Mr. Robot about '+toon+'" width="16px" height="13px"/></a>' 
         +(note!=''?'<br/>'+note:'')
         +'</div>'
     );
