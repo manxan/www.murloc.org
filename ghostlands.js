3708d3e53971ecfd33c8a7c92c797938665bceb7
apokryfos <apokryfos@ymail.com>
2013-02-19 10:41:31 +0000

    Reordered character information

--- ghostlands.js
+++ ghostlands.js
@@ -19,17 +19,20 @@ var go=function(obj) {
     }   
     document.write(
           '<div id='+toon+' class="post">'
-        + '<div id="Loading'+toon+'">'
+        + '<div id="Loading'+toon+'">'                                                  //Placeholder div, avatar and title go here
         +'<h2>'+toon+'<h2>'
         +'<img src="placeholder.png" alt=img>'
         +'</div>'
-        +'<div class="post-role">'+s+'</div>'    
+        +'<div class="post-role">'+s+'</div>'                                           //Role    
+        +(note!=''?'<div class="post-role">'+note+'</div>':'')                          //Note (if any)
+         +'<p id="'+toon+'-ilvl" class="post-role">iLvl: '                              //Average Equipped Item Level placeholder
+        +'?/?'
+        +'</p>' 
         +'<br/><a href="http://eu.battle.net/wow/en/character/Ghostlands/'+toon+'/advanced">'
         +'<img src="blizz.png" alt="battle.net" title="Official profile of '+toon+' in the wow armory at Battle.net" width="28px" height="13px"/></a>&nbsp;'
         +'<a href=http://www.wow-heroes.com/character/eu/Ghostlands/'+toon+'/>'
         +'<img src="wowheroes.png" alt="WoW-Heroes" title="Profile of '+toon+' at WoW-Heroes" width="24px" height="13px"/></a>&nbsp;'        
-        +'<a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'><img src="teamrobot.png" alt="Ask Mr. Robot" title="Ask Mr. Robot about '+toon+'" width="16px" height="13px"/></a>' 
-        +(note!=''?'<div class="post-role">'+note+'</div>':'')
+        +'<a href=http://www.askmrrobot.com/wow/gear/eu/ghostlands/'+toon+'><img src="teamrobot.png" alt="Ask Mr. Robot" title="Ask Mr. Robot about '+toon+'" width="16px" height="13px"/></a>'         
         +'</div>'
     );
     var scr = document.createElement('script');
@@ -47,12 +50,16 @@ var bnetreturn=function(o) {
             s=sprintf( o.titles[i].name,toon);
         }      
     } 
+    document.getElementById(toon+'-ilvl').innerHTML = 
+        'iLvl:'
+        +(o.items.averageItemLevel?o.items.averageItemLevel:'?')
+        +'/' 
+        +(o.items.averageItemLevelEquipped?o.items.averageItemLevelEquipped:'?');
+        
     document.getElementById('Loading'+toon).innerHTML = 
-        '<h2>'+s+'</h2>'                                                                //Toon Name        
-        +'<a href="http://eu.battle.net/wow/en/character/Ghostlands/'+toon+'/advanced">'//Armory Link
-        +'<img src="http://eu.battle.net/static-render/eu/'+o.thumbnail+'" alt=img></a>'//Avatar  
-        +'<p class="post-role">Equipped iLvl: '                                         //Average Equipped Item Level
-        +(o.items.averageItemLevelEquipped?o.items.averageItemLevelEquipped:'?')+'</p>';//Equipped    
+        '<h2>'+s+'</h2>'                                                                 //Toon Name        
+        +'<a href="http://eu.battle.net/wow/en/character/Ghostlands/'+toon+'/advanced">' //Armory Link
+        +'<img src="http://eu.battle.net/static-render/eu/'+o.thumbnail+'" alt=img></a>';//Avatar  
 };
 
 
